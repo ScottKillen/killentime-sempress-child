e028c13484adbfffdd87842be8d28c3ace995143
Scott Killen <scott@scottkillen.com>
2023-10-16 00:55:46 -0400

    Revise audio post format

--- content-audio.php
+++ content-audio.php
@@ -8,9 +8,20 @@ defined('ABSPATH') || exit;
 ?>
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_semantics('post'); ?>>
-	<div class="entry-content e-content" itemprop="description articleBody">
-		<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'sempress')); ?>
-	</div><!-- .entry-content -->
+
+	<?php if (is_search() || is_home() || is_archive()) : // Only display Excerpts in these cases
+	?>
+		<div class="entry-summary p-summary" itemprop="description">
+			<?php child_the_post_thumbnail('<div class="entry-media">', '</div>'); ?>
+			<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'sempress')); ?>
+		</div><!-- .entry-summary -->
+	<?php else : ?>
+		<div class="entry-content e-content" itemprop="description">
+			<?php child_the_post_thumbnail('<div class="entry-media">', '</div>'); ?>
+			<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'sempress')); ?>
+			<?php wp_link_pages(array('before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'sempress') . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>')); ?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
 
 	<?php get_template_part('entry', 'footer'); ?>
 </article><!-- #post-<?php the_ID(); ?> -->
