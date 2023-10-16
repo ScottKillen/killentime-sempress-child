a328ce2006ed8b9660b4af1b862ca0bc6788f09a
Scott Killen <scott@scottkillen.com>
2023-10-16 00:33:53 -0400

    Remove icon from audio posts

--- content-audio.php
+++ content-audio.php
@@ -8,15 +8,8 @@ defined('ABSPATH') || exit;
 ?>
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_semantics('post'); ?>>
-	<div class="entry-content e-content d-flex flex-row" itemprop="description articleBody">
-		<div class="icon">
-			<svg class="bi">
-				<use xlink:href="#fa-volume" />
-			</svg>
-		</div />
-		<div class="audio">
-			<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'sempress')); ?>
-		</div>
+	<div class="entry-content e-content" itemprop="description articleBody">
+		<?php the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'sempress')); ?>
 	</div><!-- .entry-content -->
 
 	<?php get_template_part('entry', 'footer'); ?>
