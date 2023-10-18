788b3c514e39509ce5be92586fbadb34eba29a67
Scott Killen <scott@scottkillen.com>
2023-10-17 21:12:42 -0400

    update to best practices

--- icons.php
+++ icons.php
@@ -5,7 +5,7 @@ add_action('wp_body_open', 'the_icons');
 function the_icons()
 {
 ?>
-	<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+	<svg display="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 		<symbol id="fa-chevron-right" viewBox="0 0 320 512">
 			<path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
 		</symbol>
