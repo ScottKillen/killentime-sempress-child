978df882039c16a07552bf79fef540ac27b929ea
Scott Killen <scott@scottkillen.com>
2023-10-19 11:50:54 -0400

    Format page

---
+++ content-page.php
@@ -0,0 +1,25 @@
+<?php
+defined('ABSPATH') || exit;
+?>
+
+<article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_semantics('page'); ?>>
+	<?php get_template_part('entry', 'header'); ?>
+
+	<?php
+	if (is_search()) : // Only display Excerpts for Search
+	?>
+		<div class="entry-summary p-summary" itemprop="description">
+			<?php custom_post_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	<?php else : ?>
+		<?php sempress_the_post_thumbnail('<div class="entry-media">', '</div>'); ?>
+
+		<div class="entry-content e-content" itemprop="description text">
+			<?php the_content(); ?>
+			<?php wp_link_pages(array('before' => '<div class="page-link">' . __('Pages:', 'sempress'), 'after' => '</div>')); ?>
+			<?php edit_post_link(__('Edit', 'sempress'), '<span class="edit-link">', '</span>'); ?>
+		</div><!-- .entry-content -->
+	<?php
+	endif;
+	?>
+</article><!-- #post-<?php the_ID(); ?> -->
